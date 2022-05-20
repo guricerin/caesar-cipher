f04c3c50b5a0c3d67a8f7b96599f8dab5a8868c2
guricerin <chanbo1e9@gmail.com>
2022-05-20 20:54:26 +0900

    impl button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,38 @@
 import React, { useState } from 'react'
-import { decrypt } from './caesar'
+import { bruteforce, decrypt } from './caesar'
 import './css/App.css'
 
 type ResultTextAreaProps = {
-  text: string
+  results: string[]
 }
 
 const ResultTextArea: React.FC<ResultTextAreaProps> = props => {
   return (
-    <>
-    </>
+    <ul>
+      {props.results.map((res, idx) => {
+        return (
+          <li>
+            <p>{idx + 1} right shift</p>
+            <textarea
+              className='code-text'
+              value={res} />
+          </li>
+        )
+      })}
+    </ul>
   )
 }
 
 function App() {
   const [input, setInput] = useState('')
-  const [result, setResult] = useState('')
+  const [results, setResults] = useState<string[]>([])
 
-  function test() {
-    const dec = decrypt(input, 1)
-    setResult(dec)
+  function onBruteForce() {
+    setResults(bruteforce(input))
+  }
+
+  function onClear() {
+    setResults([])
   }
 
   return (
@@ -27,24 +40,34 @@ function App() {
       <section className='section sf-site-content'>
         <div className='columns'>
           <div className='column'>
-            <p>Input</p>
+            <h1 className='title'>Input</h1>
             <textarea
               className='code-text'
               placeholder='Input code you want to decrypt.'
               value={input}
               onChange={(e) => setInput(e.target.value)} />
-            <button
-              className='button is-info'
-              onClick={() => test()}>
-              Brute Force
-            </button>
+            <nav className='level'>
+              <div className='level-left'>
+                <div className='level-item'>
+                  <button
+                    className='button is-info'
+                    onClick={() => onBruteForce()}>
+                    Brute Force
+                  </button>
+                </div>
+                <div className='level-item'>
+                  <button
+                    className='button is-info'
+                    onClick={() => onClear()}>
+                    Clear
+                  </button>
+                </div>
+              </div>
+            </nav>
           </div>
           <div className='column'>
-            <p>Result</p>
-            <textarea
-              className='code-text'
-              value={result}
-              onChange={(e) => setResult(e.target.value)} />
+            <h1 className='title'>Result</h1>
+            <ResultTextArea results={results} />
           </div>
         </div>
       </section>
