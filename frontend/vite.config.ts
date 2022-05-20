582a90ccbea1f870c952d173f7b0768872520245
guricerin <chanbo1e9@gmail.com>
2022-05-20 22:43:27 +0900

    use github.action for gh-pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/caesar-cipher/", // https://ja.vitejs.dev/guide/static-deploy.html#github-pages
   plugins: [react()],
   server: {
     // https://github.com/vitejs/vite/discussions/3396
